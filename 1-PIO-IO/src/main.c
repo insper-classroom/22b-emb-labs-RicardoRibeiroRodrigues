ac3ae7f691c1d25bf2212386e03ee884e067681f
Ricardo Ribeiro Rodrigues <ricardorr7@al.insper.edu.br>
2022-08-22 17:36:48 -0300

    Lab feito

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -23,10 +23,17 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define LED_PIO      PIOC     
+#define LED_PIO_ID    ID_PIOC
+#define LED_PIO_IDX   8    
+#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)
+
+// Configuracoes do botao
+#define BUT_PIO   PIOA
+#define BUT_PIO_ID ID_PIOA
+#define BUT_PIO_IDX 11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX) // esse já está pronto.
+
 
 /************************************************************************/
 /* constants                                                            */
@@ -53,7 +60,24 @@ void init(void);
 // Função de inicialização do uC
 void init(void)
 {
+	 // Initialize the board clock
+	 sysclk_init();
 
+	 // Desativa WatchDog Timer
+	 WDT->WDT_MR = WDT_MR_WDDIS;
+	 
+	 // Ativa o PIO na qual o LED foi conectado
+	 // para que possamos controlar o LED.
+	 pmc_enable_periph_clk(LED_PIO_ID);
+	 
+	 pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	 
+	 // Inicializa PIO do botao
+	 pmc_enable_periph_clk(BUT_PIO_ID);
+	
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+
+	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
 }
 
 /************************************************************************/
@@ -69,7 +93,17 @@ int main(void)
   // aplicacoes embarcadas não devem sair do while(1).
   while (1)
   {
-
+	  if (!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)) {
+		  for (int i = 0; i < 5; i++){
+			  pio_set(LED_PIO, LED_PIO_IDX_MASK);
+			  delay_ms(200);
+			  pio_clear(LED_PIO, LED_PIO_IDX_MASK);
+			  delay_ms(200);
+		  }
+		 pio_set(LED_PIO, LED_PIO_IDX_MASK);
+		 delay_ms(200);
+	  }
+	  
   }
   return 0;
 }
