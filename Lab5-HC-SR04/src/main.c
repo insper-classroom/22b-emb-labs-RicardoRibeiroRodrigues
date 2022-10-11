f3718e231fe4d78ac66d690d5ea7fc748870f79c
RicardoRibeiroRodrigues <riywar@gmail.com>
2022-10-10 22:51:39 -0300

    Lab 5 finalizado e funcionando

--- Lab5-HC-SR04/src/main.c
+++ Lab5-HC-SR04/src/main.c
@@ -23,9 +23,11 @@
 #define ECHO_PIO_IDX 6
 #define ECHO_PIO_IDX_MASK (1u << ECHO_PIO_IDX)
 
-#define V_SOM 340
+#define V_SOM 340.0
 #define WIDTH_OLED 128
 #define HEIGHT_OLED 32
+// 1 / tempo mínimo que echo pode ficar em 1
+#define FREQ 1 / (2 * 0.000058)
 
 
 
@@ -41,12 +43,17 @@ extern void xPortSysTickHandler(void);
 
 xQueueHandle xQueueEcho;
 
+// Para atualizar o Oled a cada 1 segundo
+xTimerHandle xTimerOled;
+xSemaphoreHandle xSemaphoreOled;
+
 /** prototypes */
 static void io_init(void);
 static void RTT_init(float freqPrescale, uint32_t IrqNPulses, uint32_t rttIRQSource);
 void send_trig(void);
 void gfx_clear(void);
 void oled_dist(double dist);
+void desenha_grafico_e_numero(float dist, int *x_atual);
 
 
 /************************************************************************/
@@ -71,15 +78,32 @@ extern void vApplicationMallocFailedHook(void) {
 /************************************************************************/
 
 void echo_callback(void) {
-	// Para borda de subida
 	if (pio_get(ECHO_PIO, PIO_INPUT, ECHO_PIO_IDX_MASK)) {
 		printf("Borda de subida\n");
-		RTT_init(40000, 0, 0);
+		// Alarme no 204 -> O dt já é maior que o máximo possível.
+		RTT_init(FREQ, 204, RTT_MR_ALMIEN);
 	} else {
 		printf("Borda de descida\n");
 		uint32_t timer_value = rtt_read_timer_value(RTT);
+		printf("Timer value = %u\n", timer_value);
 		xQueueSendFromISR(xQueueEcho, &timer_value, 0);
 	}
+}
+
+void RTT_Handler(void) {
+	uint32_t ul_status;
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		printf("Estorou o alarme!");
+		uint32_t timer_value = rtt_read_timer_value(RTT);
+		xQueueSendFromISR(xQueueEcho, &timer_value, 0);
+	}
+}
+
+void vTimerOledCallback(TimerHandle_t xTimer) {
+	xSemaphoreGiveFromISR(xSemaphoreOled, 0);
 }
 
 /************************************************************************/
@@ -89,21 +113,52 @@ void echo_callback(void) {
 static void task_oled(void *pvParameters) {
 	gfx_mono_ssd1306_init();
 	gfx_mono_draw_string("Distancia atual: ", 0, 0, &sysfont);
+	float dist = 0;
+	int x_atual = 0;
 	
 	send_trig();
 	uint32_t dT;
+	char erro = 0;
+	
+	 xTimerOled = xTimerCreate(
+		 "TimerOled",
+		 250 / portTICK_PERIOD_MS, // 1/4 segundo
+		 pdTRUE,
+		 (void *) 0,
+		 vTimerOledCallback
+	 );
+	 xTimerStart(xTimerOled, 0);
 
 	for (;;)  {
 		if (xQueueReceive(xQueueEcho, &dT, 0)) {
-			printf("Entra no receive\n");
-			printf("%u\n", dT);
-			float dt_s = dT / 40000;
-			float dist = (dt_s * V_SOM) / 2;
-			printf("dist = %f\n", dist);
-			gfx_clear();
-			gfx_mono_draw_string("Distancia atual: ", 0, 0, &sysfont);
-			oled_dist(dist);
+			printf("dt = %lu\n", dT);
+			dist = (V_SOM * dT) / (2 * FREQ) * 100; // * 100 para converter para centimetros
+			
+			if (dist > 400) {
+				// Erro de espaco aberto
+				x_atual = 0;
+				erro = 'a';
+			} else if(dist < 2) {
+				erro = 'm';
+			} else {
+				erro = 0;
+			}
+			printf("%.6lf\n", dist);
+			// oled_dist(dist);
 			send_trig();
+		}
+		// Esse semaforo serve para atualizar o OLED um pouco mais lento, para nao fazer a atualizacao rapido demais para dar para ver
+		if (xSemaphoreTake(xSemaphoreOled, 0)) {
+			if (!erro) {
+				// oled_dist(dist);
+				desenha_grafico_e_numero(dist, &x_atual);
+			} else if (erro == 'a') {
+				gfx_clear();
+				gfx_mono_draw_string("Espaco aberto", 0, 0, &sysfont);
+			} else if (erro == 'm') {
+				gfx_clear();
+				gfx_mono_draw_string("Mau contato", 0, 0, &sysfont);
+			}
 		}
 	}
 }
@@ -113,8 +168,10 @@ static void task_oled(void *pvParameters) {
 /************************************************************************/
 
 void oled_dist(double dist) {
+	gfx_clear();		
+	gfx_mono_draw_string("Distancia atual: ", 0, 0, &sysfont);
 	char str[128];
-	sprintf(str, "%.6lf m", dist);
+	sprintf(str, "%.2lf cm", dist);
 	gfx_mono_draw_string(str, 0, 16, &sysfont);
 }
 
@@ -136,6 +193,14 @@ static void configure_console(void) {
 void gfx_clear(void) {
 	for (int x = 0; x < WIDTH_OLED; x++) {
 		for (int y = 0; y < HEIGHT_OLED; y++) {
+			gfx_mono_draw_pixel(x, y, GFX_PIXEL_CLR);
+		}
+	}
+}
+
+void gfx_clear_half(void) {
+	for (int x = 0; x < WIDTH_OLED; x++) {
+		for (int y = 0; y < HEIGHT_OLED / 2; y++) {
 			gfx_mono_draw_pixel(x, y, GFX_PIXEL_CLR);
 		}
 	}
@@ -197,7 +262,7 @@ static void io_init(void) {
 	pio_get_interrupt_status(ECHO_PIO);
 	
 	NVIC_EnableIRQ(ECHO_PIO_ID);
-	NVIC_SetPriority(ECHO_PIO_ID, 5);
+	NVIC_SetPriority(ECHO_PIO_ID, 4);
 	
 	// trigger pin
 	pio_set_output(TRIG_PIO, TRIG_PIO_IDX_MASK, 0, 0, 1);
@@ -205,8 +270,31 @@ static void io_init(void) {
 
 void send_trig(void) {
 	pio_set(TRIG_PIO, TRIG_PIO_IDX_MASK);
-	delay_us(12);
+	delay_us(10);
 	pio_clear(TRIG_PIO, TRIG_PIO_IDX_MASK);
+}
+
+void desenha_grafico_e_numero(float dist, int *x_atual)
+{
+	char str[20];
+	sprintf(str, "%.1f cm", dist);
+	gfx_clear_half();
+	gfx_mono_draw_string(str, 0, 0, &sysfont);
+	
+	
+
+	if (*x_atual == 0)
+	gfx_mono_generic_draw_filled_rect(70, 0, 74, 31, GFX_PIXEL_CLR);
+	
+	int dist_max = 400; // cm
+	int dist_min = 2; // cm
+	int diff = dist_max - dist_min;
+	
+	// Comecando em y = 1 do oled e terminado em y = 30 -> 30 valores
+	// Usa o 29 e 30.5 no primeiro e no segundo por conta da divisao inteira ser floor
+	int pos = ((-29 * dist) + (diff * 30.5)) / (diff);
+	gfx_mono_generic_draw_horizontal_line(*x_atual + 70, pos, 1, GFX_PIXEL_SET);
+	*x_atual = *x_atual >= 58 ? 0 : *x_atual + 1;
 }
 
 /************************************************************************/
@@ -231,6 +319,10 @@ int main(void) {
 	xQueueEcho = xQueueCreate(32, sizeof(uint32_t));
 	if (xQueueEcho == NULL)
 		printf("Falha ao criar Queue\n");
+	
+	xSemaphoreOled = xSemaphoreCreateBinary();
+	if (xSemaphoreOled == NULL)
+		printf("Falha ao criar semaforo\n");
 		
 	/* Start the scheduler. */
 	vTaskStartScheduler();
